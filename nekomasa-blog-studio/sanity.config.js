5d46ad40044afa5bd9a5d1ed0f1fcfd74bb76081
Yukimasa8 <222359572+Yukimasa8@users.noreply.github.com>
2025-09-17 00:39:33 +0900

    Sanity更新

--- nekomasa-blog-studio/sanity.config.js
+++ nekomasa-blog-studio/sanity.config.js
@@ -10,7 +10,31 @@ export default defineConfig({
   projectId: 'xm3dmjar',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('Content')
+          .items([
+            S.listItem()
+              .title('Post')
+              .child(S.documentTypeList('post').title('Post')),
+            S.listItem()
+              .title('Author')
+              .child(S.documentTypeList('author').title('Author')),
+            S.listItem()
+              .title('Category')
+              .child(S.documentTypeList('category').title('Category')),
+            S.listItem()
+              .title('Affiliate')
+              .child(S.documentTypeList('affiliate').title('Affiliate')),
+            S.listItem()
+              .title('Tag')
+              .child(S.documentTypeList('tag').title('Tag')),
+          ]),
+    }),
+    visionTool(),
+  ],
 
   schema: {
     types: schemaTypes,
