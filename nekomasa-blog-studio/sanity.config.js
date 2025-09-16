08062742cf39a2755a079dbec7bb0fd5373fd7d1
Yukimasa8 <222359572+Yukimasa8@users.noreply.github.com>
2025-09-17 00:46:23 +0900

    タグ表示

--- nekomasa-blog-studio/sanity.config.js
+++ nekomasa-blog-studio/sanity.config.js
@@ -17,18 +17,6 @@ export default defineConfig({
           .title('Content')
           .items([
             S.listItem()
-              .title('Post')
-              .child(S.documentTypeList('post').title('Post')),
-            S.listItem()
-              .title('Author')
-              .child(S.documentTypeList('author').title('Author')),
-            S.listItem()
-              .title('Category')
-              .child(S.documentTypeList('category').title('Category')),
-            S.listItem()
-              .title('Affiliate')
-              .child(S.documentTypeList('affiliate').title('Affiliate')),
-            S.listItem()
               .title('Tag')
               .child(S.documentTypeList('tag').title('Tag')),
           ]),
