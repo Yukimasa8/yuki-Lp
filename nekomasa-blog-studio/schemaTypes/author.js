291bb60b920a985383719ae27628d82c052dc09a
Yukimasa8 <222359572+Yukimasa8@users.noreply.github.com>
2025-09-22 15:48:45 +0900

    Revert "Fix: Temporarily simplify author schema bio field to debug React error"
    This reverts commit 4dd396c7f56a821949c54dfc797ee24709bfe8af.

--- nekomasa-blog-studio/schemaTypes/author.js
+++ nekomasa-blog-studio/schemaTypes/author.js
@@ -32,7 +32,17 @@ export default {
       type: 'array',
       of: [
         {
+          title: 'Block',
           type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+          marks: {
+            decorators: [
+              {title: 'Strong', value: 'strong'},
+              {title: 'Emphasis', value: 'em'},
+            ],
+            annotations: [],
+          },
         },
       ],
     },
