db72af8a0f0f9e2959b5b61d479c0011fea6305a
Yukimasa8 <222359572+Yukimasa8@users.noreply.github.com>
2025-09-24 00:45:49 +0900

    "FixSanityCliHostname"

--- nekomasa-blog-studio/sanity.cli.js
+++ nekomasa-blog-studio/sanity.cli.js
@@ -9,5 +9,8 @@ export default defineCliConfig({
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
-  deployment: {autoUpdates: true},
+  deployment: {
+    autoUpdates: true,
+    hostname: 'nekomasa-blog'
+  },
 })
